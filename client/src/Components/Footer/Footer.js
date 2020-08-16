e28252e7febc4e54d82c48768a5a0a92484ba5d5
mohur <m.ashrahfi@gmail.com>
2020-08-15 18:28:53 -0700

    Updating footer

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -11,6 +11,18 @@ const Footer = () => {
             |
         </div>
         <div className={styles.footerContent}>
+            <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/donate">Donate</a>
+        </div>
+        <div>
+            |
+        </div>
+        <div className={styles.footerContent}>
+            <a href="https://www.cdc.gov/coronavirus/2019-ncov/lab/faqs.html">More Info on COVID19</a>
+        </div>
+        <div>
+            |
+        </div>
+        <div className={styles.footerContent}>
             <a href="https://github.com/mohur"><img  className={styles.logo}src="https://image.flaticon.com/icons/png/512/25/25231.png"></img></a>
         </div>
     </div>
