562e0b17505b04179edd074b76c6b259ad6bf247
mohur <m.ashrahfi@gmail.com>
2020-08-31 19:36:21 -0700

    setting up express backend

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server has started on port ${port}`));
