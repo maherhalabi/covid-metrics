10164277158c5dcde3337fb7e4aad771044aa102
mohur <m.ashrahfi@gmail.com>
2021-11-14 19:50:24 -0800

    push .git

--- client/src/Components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Cards } from './Cards/Cards';
-export { default as Charts } from './Charts/Charts';
-export { default as CountryPicker } from './CountryPicker/CountryPicker';
-export { default as Title } from './Title/Title';
-export { default as Footer } from './Footer/Footer';
-export { default as LastUpdated } from './LastUpdated/LastUpdated';
